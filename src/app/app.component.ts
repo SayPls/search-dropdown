fb59bda0c87f466e6d8af125e70cbde70989f452
SayPlease <saypleaze@outlook.com>
2022-05-05 18:51:23 +0300

    add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
 
+export const fadeAnimation = trigger('fadeAnimation', [
+  transition(':enter', [
+    style({ width: 0 }), animate('300ms', style({ width: '70%' }))]
+  ),
+  transition(':leave',
+    [style({ width: '70%' }), animate('300ms', style({ width: 0 }))]
+  )
+]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [fadeAnimation]
 })
-export class AppComponent {
-  title = 'search-dropdown';
+export class AppComponent implements OnInit{
+  width!: number;
+  search: boolean = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.width = event.target.innerWidth;
+  }
+
+  ngOnInit(): void {
+    this.width = window.innerWidth;
+  }
+
+  triggerSearch() {
+    this.search =  !this.search
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -10,9 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
